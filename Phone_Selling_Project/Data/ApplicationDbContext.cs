12837e1730709927d020ed5c89491a25886f3fca
LpSymons <71932780+LpSymons@users.noreply.github.com>
2021-11-15 16:41:17 +0000

    Add project files.

---
+++ Phone_Selling_Project/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Phone_Selling_Project.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
